9f8fa5179fb366379e501ce79bfc8c5a7cd03f53
David McGuffey <dmcguffey94@gmail.com>
2023-02-26 13:25:46 -0600

    You can now add balance to your account.

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount main = new BankAccount();
+
+            Console.WriteLine($"Welcome. Please determine how much you would like to deposit.");
+            double deposit = int.Parse(Console.ReadLine());
+            main.Deposit(deposit);
+            Console.WriteLine($"You now have ${main.GetBalance()} in your account.");
         }
     }
 }
